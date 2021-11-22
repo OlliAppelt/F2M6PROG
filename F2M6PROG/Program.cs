11af2e2a4111007547af476567cd8455a6f841d9
Olli Appelt <30849@ma-web.nl>
2021-11-22 13:39:04 +0000

    first commit

---
+++ F2M6PROG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace F2M6PROG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
